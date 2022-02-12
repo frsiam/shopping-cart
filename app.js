cbc3ee255b19ed7182509f26b1355b66f038aa58
frsiam <siam.frs@gmail.com>
2022-02-12 23:02:29 +0600

    add event listener into the positive button of phone section

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('I am in a separate file')
+let phoneInfo = document.getElementById('phone');
+let phoneQuantity = parseInt(phoneInfo.value);
+document.getElementById('phone-plus').addEventListener('click',function(){
+    phoneQuantity++;
+    phoneInfo.value = phoneQuantity;
+    console.log(phoneQuantity);
+})
