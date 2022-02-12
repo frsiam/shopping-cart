fb5214b050286637570005d2c2cce6a56f757a88
frsiam <siam.frs@gmail.com>
2022-02-12 23:37:57 +0600

    phone total bill calculation done

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 function updateQuantity(isAdd){
     let phoneInfo = document.getElementById('phone');
     let phoneQuantity = parseInt(phoneInfo.value);
+
+    let phoneOutput = document.getElementById('phone-total');
+    let phonePrice = parseFloat(phoneOutput.innerText);
     if(isAdd){
         phoneQuantity++
+        phonePrice = 1219 * phoneQuantity;
     }
     else{
         if(phoneQuantity > 0){
             phoneQuantity--;
+            phonePrice = 1219 * phoneQuantity;
         }
         else{
             alert('sorry');
         }
     }
     phoneInfo.value = phoneQuantity;
+    phoneOutput.innerText = phonePrice;
 }
 document.getElementById('phone-plus').addEventListener('click',function(){
     updateQuantity(true)
